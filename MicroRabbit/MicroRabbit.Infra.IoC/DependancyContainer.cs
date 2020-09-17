167d20f9f64b8216ff76bb9501c105b833c47397
MahmoudElderby <MahmoudElderby@DESKTOP-6C1EG06>
2020-09-17 14:06:49 +0200

    Commit

--- MicroRabbit/MicroRabbit.Infra.IoC/DependancyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependancyContainer.cs
@@ -1,7 +1,5 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
-using MicroRabbit.Banking.Data.Context;
-using MicroRabbit.Banking.Data.Repository;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
